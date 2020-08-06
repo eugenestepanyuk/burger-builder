31af1e6f1840606705ba275492c05956041b4710
eugenestepanyuk <erl.evgeniy@gmail.com>
2020-08-06 18:08:53 +0300

    Changing the Folder Structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
@@ -14,7 +14,7 @@ const orderSummary = (props) => {
         });
 
     return (
-        <Aux>           
+        <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
